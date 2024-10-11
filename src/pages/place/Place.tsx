889d0ee82718db00c065662854053e423551047a
ALotUsGit <arjeon.career@gmail.com>
2024-10-12 02:05:17 +0900

    장소 페이지 레이아웃 작업 (필터 미완성)

--- src/pages/place/Place.tsx
+++ src/pages/place/Place.tsx
@@ -4,6 +4,15 @@ import { Response, fetchPlaces } from "../../requests/place/fetchPlaces";
 import MainComponent from "../../components/MainComponent";
 import PlaceItem from "../../components/PlaceItem";
 import LoadPlaceItem from "../../components/loading/LoadPlceItem";
+import { NavLink } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
+import {
+  ChevronRightIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
+import Button from "../../components/form/Button";
+
+const Tabs = ["popular", "new"];
 
 const Place = () => {
   const [loading, setLoding] = useState(true);
@@ -25,8 +34,21 @@ const Place = () => {
   }, []);
 
   return (
-    <MainComponent>
+    <MainComponent className="mt-10 mb-20">
       <section>
+        <div className="flex items-center gap-4 mb-6">
+          {Tabs.map((tab) => (
+            <NavLink
+              key={tab}
+              to={`/place?tab=${tab}`}
+              className={twMerge(
+                "pr-4 text-gray-400 text-2xl/none font-light border-r border-gray-400 last:padding last:border-none"
+              )}
+            >
+              {tab.toUpperCase()}
+            </NavLink>
+          ))}
+        </div>
         <ul className="grid grid-cols-5 gap-x-10">
           {loading ? (
             <LoadPlaceItem />
@@ -34,6 +56,45 @@ const Place = () => {
             placeData.map((data) => <PlaceItem key={data.id} data={data} />)
           )}
         </ul>
+      </section>
+
+      <div className="flex items-center gap-4 my-20 mx-auto py-5 px-10 w-4/5 border border-gray-400 rounded-full">
+        <input
+          type="text"
+          placeholder="어떤 공간을 찾고 계신가요?"
+          className="flex-1 p-0 text-xl/none border-0 focus:ring-transparent placeholder:text-gray-400"
+        />
+        <MagnifyingGlassIcon className="size-6" />
+      </div>
+
+      <section className="mb-20">
+        <div className="flex gap-3">
+          <Button>사용목적</Button>
+          <Button>필터</Button>
+          <Button className="ml-auto">추천순</Button>
+        </div>
+
+        <ul className="grid grid-cols-5 gap-x-10 gap-y-14 mt-10 mb-16">
+          {loading ? (
+            <LoadPlaceItem />
+          ) : (
+            placeData.map((data) => <PlaceItem key={data.id} data={data} />)
+          )}
+        </ul>
+
+        <Button className="mx-auto">더보기</Button>
+      </section>
+
+      <section>
+        <h2 className="text-4xl text-center font-bold">
+          장소 등록을 원하시나요?
+        </h2>
+
+        <div className="overflow-hidden flex justify-center items-center h-80 mt-10 bg-gray-200 rounded-2xl">
+          <Button>
+            장소 등록 바로가기 <ChevronRightIcon className="size-4" />
+          </Button>
+        </div>
       </section>
     </MainComponent>
   );
